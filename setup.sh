1d55ed328e44c43d380c06f256d503c6e3bd4955
shailesh-zx <138345328+zx-hackers@users.noreply.github.com>
2024-05-04 20:13:34 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt install tor -y
+pip install requests
+pip install requests[socks] 
+pip install stem
+sudo pip3 install instagram-py --upgrade
+mv instapy-config.json ~
+rm  /etc/tor/torrc
+mv torrc /etc/tor/
+cd
